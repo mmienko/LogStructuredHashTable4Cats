e3dbb280802bba9f1bd66330fde2970f6e5d1d34
Michael Mienko <michaelmienko@gmail.com>
2024-05-14 20:39:57 -0400

    Add Stack Trace back into errors

--- src/main/scala/io/lsht/Errors.scala
+++ src/main/scala/io/lsht/Errors.scala
@@ -1,6 +1,7 @@
 package io.lsht
 
 import cats.syntax.option.*
+import fs2.io.file.Path
 
 import scala.util.control.NoStackTrace
 
@@ -9,30 +10,35 @@ object Errors {
   class StartupException extends Throwable
 
   object Startup {
-    object PathNotADirectory extends StartupException with NoStackTrace
-    object BadChecksum extends StartupException with NoStackTrace
-    object MissingTombstoneKey extends StartupException with NoStackTrace
-    object MissingValueSize extends StartupException with NoStackTrace
-    object MissingKeyValueEntry extends StartupException with NoStackTrace
+    object PathNotADirectory extends StartupException
+    object BadChecksum extends StartupException
+    object MissingTombstoneKey extends StartupException
+    object MissingValueSize extends StartupException
+    object MissingKeyValueEntry extends StartupException
   }
 
   class ReadException(cause: Option[Throwable]) extends Throwable(cause.orNull)
 
   object Read {
-    object BadChecksum extends ReadException(cause = None) with NoStackTrace
+    object BadChecksum extends ReadException(cause = None)
 
-    object CorruptedDataFile extends ReadException(cause = None) with NoStackTrace
+    object CorruptedDataFile extends ReadException(cause = None)
 
-    final case class FileSystem(cause: java.nio.file.FileSystemException)
-        extends ReadException(cause.some)
-        with NoStackTrace
+    final case class FileSystem(cause: java.nio.file.FileSystemException) extends ReadException(cause.some)
   }
 
   class WriteException(cause: Option[Throwable]) extends Throwable(cause.orNull)
 
   object Write {
-    class Failed(cause: Throwable) extends WriteException(cause.some) with NoStackTrace
-    object Cancelled extends WriteException(cause = None) with NoStackTrace
-    object CancelledButSavedToDisk extends WriteException(cause = None) with NoStackTrace
+    class Failed(cause: Throwable) extends WriteException(cause.some)
+    object Cancelled extends WriteException(cause = None)
+    object CancelledButSavedToDisk extends WriteException(cause = None)
+  }
+
+  class CompactionException(message: String) extends Throwable(message)
+
+  object CompactionException {
+    class SeekAndReadFailedOnDataFile(file: Path, offset: Offset)
+        extends CompactionException(s"Could not read Corrupted DataFile ${file.toString} at offset ${offset.toString}")
   }
 }
